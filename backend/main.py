5dd9211dfc5bd7ca0b45e7064684a57cc9c48c69
hasansk98 <shaik.hasan78422@gmail.com>
2026-08-13 10:44:26 +0530

    Allow Vercel frontend in backend CORS

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://my-ats-app-16.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
